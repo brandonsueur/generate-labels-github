cf0bf0405012253ed524d77f99a54398d6b569c3
brandonsueur <gynidark@gmail.com>
2019-05-17 16:18:33 +0200

    feat: add labels

---
+++ labels.js
@@ -0,0 +1,67 @@
+[
+  {
+    "name" : "priority: high",
+    "description": "",
+    "color": "#ca4b25"
+  },
+  {
+    "name" : "priority: low",
+    "description": "",
+    "color": "#76caa6"
+  },
+  {
+    "name" : "priority: maximum",
+    "description": "",
+    "color": "#aa2119"
+  },
+  {
+    "name" : "priority: medium",
+    "description": "",
+    "color": "#f6ca46"
+  },
+  {
+    "name" : "source: back ⚙️",
+    "description": "",
+    "color": "#2e6ec1"
+  },
+  {
+    "name" : "source: front 🎨",
+    "description": "",
+    "color": "#4b28e1"
+  },
+  {
+    "name" : "type: bug 🐛",
+    "description": "",
+    "color": "#eb4037"
+  },
+  {
+    "name" : "type: discussion ✍️",
+    "description": "",
+    "color": "#d1c6f2"
+  },
+  {
+    "name" : "type: documentation 📖",
+    "description": "",
+    "color": "#2a6975"
+  },
+  {
+    "name" : "type: enhancement 💅",
+    "description": "",
+    "color": "#491cb2"
+  },
+  {
+    "name" : "type: feature request 🙏",
+    "description": "",
+    "color": "#8ab6e7"
+  },
+  {
+    "name" : "type: help wanted 🤷‍",
+    "description": "",
+    "color": "#66da6c"
+  },
+  {
+    "name" : "type: question 🙋",
+    "description": "",
+    "color": "#bd407a"
+  }
+]
