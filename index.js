32c710ab51d8e5539e6472951620968be96588be
brandonsueur <gynidark@gmail.com>
2019-05-20 22:01:54 +0200

    chore: add package github-api

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const GitHub = require('github-api');
 const config = require("./config.json");
+
+// basic auth
+var gh = new GitHub({
+  username: config.username,
+  password: config.password,
+  token: config.token
+});
+
+var me = gh.getUser(); 
+console.log(me);
 
 // créer un dépôt
   // créer les différents labels(labels.js)
-
-console.log(config);
