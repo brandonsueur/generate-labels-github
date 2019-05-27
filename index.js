13135bb2a4655dc1c575e384da5108429042ca7b
brandonsueur <gynidark@gmail.com>
2019-05-27 11:09:58 +0200

    refactor: remove line useless

--- index.js
+++ index.js
@@ -7,27 +7,21 @@ const me = gh.getUser()
 me.getProfile()
 .then(({data: user}) => {
   console.log(`👋🏻 Welcome ${user.name} !`, `\n`)
-})
-.then(() => {
-  // me.createRepo({'name': config.repository, 'public': false})
-  // .then(({data: repo}) => {
-  //   console.log(`✅ Le dépôt ${config.repository} est créé.`, `\n`)
-  //   console.log(`🔎 ${repo.html_url}`, `\n`)
-  // }).catch(err => console.log(err))
-})
-.then(() => {
-  remoteIssues = gh.getIssues(config.username, config.repository);
-  remoteIssues.listLabels().then(({data: labels}) => {
+  
+  remoteIssues = gh.getIssues(config.username, 'super-toto')
+
+  remoteIssues.listLabels()
+  .then(({data: labels}) => {
     let labelsArray = []
     
     labels.map(label => labelsArray.push(label.name))
-  
     for (let i = 0; i < labelsArray.length; i++) {
       remoteIssues.deleteLabel(labelsArray[i])
     }
 
-    console.log(`👌🏼 Labels par défaut supprimé !`)
-
+    console.log(`✅ Labels par défaut supprimé !`)
   })
-  .catch(err => console.log(err))
+  .then(() => {
+    // for add labels
+  })
 })
