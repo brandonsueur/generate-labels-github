a025a3ab226f47265b24cb5ff0a920422631df90
Clément <le.mikmac@gmail.com>
2019-05-28 01:05:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,46 +5,18 @@ const labelsFile = require("./labels.json")
 const gh = new GitHub({ token: config.token })
 const me = gh.getUser()
 
-me.getProfile()
-.then(({data: user}) => {
-  console.log(`👋🏻 Welcome ${user.name} !`, `\n`)
-  
-  remoteIssues = gh.getIssues(config.username, 'super-toto')
+const main = async () {
+  const { data: user } = await me.getProfile()
+  console.log(`👋🏻 Welcome ${user.name} !\n`)
 
-  remoteIssues.listLabels()
-  .then(({data: labels}) => {
-    let labelsArray = []
-    
-    if(labelsArray.length > 1){
-      labels.map(label => labelsArray.push(label.name))
-      for (let i = 0; i < labelsArray.length; i++) {
-        remoteIssues.deleteLabel(labelsArray[i])
-      }
+  const remoteIssues = gh.getIssues(config.username, 'super-toto')
+  const { data: labels } = remoteIssues.listLabels()
+  await Promise.all(labels.map(label => remoteIssues.deleteLabel(label)))
+  console.log(`✅ Labels par défaut supprimé !`)
 
-      console.log(`✅ Labels par défaut supprimé !`)
-    }
-  })
-  .then(() => {
-    label = { name: "toto", color: "red"}
-    remoteIssues.createLabel(label)
-    .then(({data: _createdLabel}) => {
-      createdLabel = label.name
-    })
+  const label = { name: "toto", color: "red"}
+  const { data: _createdLabel } = await remoteIssues.createLabel(label)
+  const createdLabel = label.name
+}
 
-    // console.log(labelsFile.length)
-    // for (let i = 0; i < labelsFile.length; i++) {
-    //   let labelsExample = {
-    //     name: labelsFile[i].name,
-    //     color: labelsFile[i].color,
-    //   }
-
-    //   console.log(labelsExample);
-
-     
-      
-      
-    //}
-    // console.log(labelsFile[])
-    
-  })
-})
+main()
